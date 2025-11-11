e7be6d388a29a3c08d35b5afe6f41a9644ca945a
Casey Hartman <casey.hartman@tufts.edu>
2025-11-11 16:22:58 +0100

    Data Analysis A3 Random Forest Project start (Data preprossessing done)

--- Astrophysics_and_Data_Analysis/Functions.py
+++ Astrophysics_and_Data_Analysis/Functions.py
@@ -14,7 +14,23 @@ def data_loader(file_path):
     Loads in a csv from a given path. The csv must be in the Databases folder to work.
     '''
     # Load CSV
-    df = pd.read_csv("C:\\Users\\casey\\UnigeMasters\\Astrophysics_and_Data_Analysis\Databases\\" + str(file_path),  usecols=['t','y', 'ey'])
+    df = pd.read_csv("C:\\Users\\casey\\UnigeMasters\\Astrophysics_and_Data_Analysis\Databases\\" + str(file_path))
     # Print dataframe to check
     # print(df.head())
     return df
+
+
+def min_maxing(features, range=0):
+    '''
+    Normalizes the data contained in features via min max scaling
+    '''
+    if range == 0:
+        return (features - features.min()) / (features.max() - features.min())
+    elif range == -1:
+        return 2* (features - features.min()) / (features.max() - features.min()) -1
+
+def z(features):
+     '''
+    Normalizes the data contained in features via standarization
+    '''
+     return (features - features.mean()) / features.std()
